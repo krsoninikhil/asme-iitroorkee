c21a49526dcb928c80a0c92ed8d87d534b140362
Nikhil Kumar Soni <krsoninikhil@gmail.com>
2015-08-12 03:49:51 +0530

    add aaa script and change wkhtmltopdf exe_path

---
+++ app/assets/javascripts/aaa_complete.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function(){
+	$("#dropdownMenuLogin").click(function(){
+		$(".widget-box").toggle();
+	});
+	$(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+		if(scroll > 0)	$("#main-navigation").addClass('opaque');
+		else $("#main-navigation").removeClass('opaque');
+	});
+	$( "section, #myCarousel" ).click(function(){
+		$(".widget-box").hide();
+	});
+});
--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,10 @@
 WickedPdf.config = {
   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
   #:layout => "pdf.html",
-  :exe_path => 'C:\wkhtmltopdf\bin\wkhtmltopdf.exe'
+  #:exe_path => 'C:\wkhtmltopdf\bin\wkhtmltopdf.exe'
+  if Rails.env == 'development'
+		:exe_path => 'C:\wkhtmltopdf\bin\wkhtmltopdf.exe'
+	else
+		:exe_path => '/usr/local/bin/wkhtmltopdf'
+	end
 }
