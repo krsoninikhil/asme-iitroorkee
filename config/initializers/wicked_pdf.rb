b9fe77750aa663cc678afb1a9cf21050208b8561
Nikhil Kumar Soni <krsoninikhil@gmail.com>
2015-08-26 02:31:56 +0530

    using CDN for images

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,6 +1,6 @@
 WickedPdf.config = {
   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
   #:layout => "pdf.html",
-  :exe_path => 'C:\wkhtmltopdf\bin\wkhtmltopdf.exe'
+  #:exe_path => 'C:\wkhtmltopdf\bin\wkhtmltopdf.exe'
   #:exe_path => '~/bin/wkhtmltopdf'
 }
